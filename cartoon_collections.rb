1f5e2e9edda367af67fbcd89ac0c8af3b19912ba
Jeremy Winn <jeremyjwinn@gmail.com>
2020-06-24 19:46:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,6 @@ def list_dwarves(array)
   # Print a numbered list of each element
   
   array.each_with_index | name, index + 1 |
-    puts array[item]
-  
+    puts array[item] = index
+  end
 end
